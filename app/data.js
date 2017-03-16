ccaf4380cc5b788579c522e717ea2f01798154d0
k8ttle <info@ixdlab.co.uk>
2017-03-16 05:19:28 -0700

    repairing prototype so single borrower is displayed

--- app/data.js
+++ app/data.js
@@ -5,9 +5,9 @@ module.exports = {
   // Set to http://govuk-verify.herokuapp.com/intro?requestId=sign-mortgage in heroku env vars
   verifyUrl: 'verify1',
   
-  name: 'Ann Theresa Smith',
-  fname: 'Ann Theresa Smith',
-  mname: 'Peter Alan Smith',
+  name: 'Ann Smith',
+  fname: 'Ann Smith',
+  mname: 'Peter Smith',
   company: 'Samuel Montagu & Co.',
   companyb: 'Samuel Montagu & Co.'
 };
